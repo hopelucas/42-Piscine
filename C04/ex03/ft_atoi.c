131d98277deaa6d33fd860685175c4cf431c456f
Hope Lucas <hopelucas17@gmail.com>
2021-12-07 17:27:34 +1030

    C04 complete to ex05

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,44 @@
+#include "libft.h"
+
+static int	ft_iswspace(int c)
+{
+	return ((c >= 9 && c <= 13) || (c == 32));
+}
+
+static int	ft_long_border(const char c, int mult, long nb)
+{
+	long	border;
+
+	border = 922337203685477580;
+	if ((nb > border || (nb == border && (c - '0') > 7)) && mult == 1)
+		return (-1);
+	else if ((nb > border || (nb == border && (c - '0') > 8)) && mult == -1)
+		return (0);
+	return (1);
+}
+
+int	ft_atoi(const char *str)
+{
+	long	num;
+	int		sign;
+	int		brd;
+
+	num = 0;
+	sign = 1;
+	brd = 1;
+	while (ft_iswspace(*str))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && ft_isdigit(*str))
+	{
+		brd = ft_long_border(*str, sign, num);
+		if (brd < 1)
+			return (brd);
+		num = (num * 10) + (*str - '0');
+		str++;
+	}
+	return (num * sign);
+}
