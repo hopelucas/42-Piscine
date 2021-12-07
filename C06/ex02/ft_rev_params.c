e0817107e86bd3f6d269e409689d93cb538b9c02
Hope Lucas <hopelucas17@gmail.com>
2021-12-07 17:40:44 +1030

    C06 complete

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,24 @@
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
