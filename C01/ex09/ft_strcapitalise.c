b3635fcbbdef4f0d9a48277ca7dd4550e7d0eedc
Hope Lucas <hopelucas17@gmail.com>
2021-12-07 17:03:49 +1030

    C02 done up to ex12

---
+++ C01/ex09/ft_strcapitalise.c
@@ -0,0 +1,24 @@
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		next;
+	char	c;
+
+	i = 0;
+	next = 1;
+	while (*(str + i) != '\0')
+	{
+		c = *(str + i);
+		if (next == 1 && c >= 'a' && c <= 'z')
+			str[i] -= 32;
+		else if (next == 0 && c >= 'A' && c <= 'Z')
+			str[i] += 32;
+		if (c < '0' || (c > '9' && c < 'A') || (c > 'Z' && c < 'a') || c > 122)
+			next = 1;
+		else
+			next = 0;
+		i++;
+	}
+	return (str);
+}
